3698caaacce2cd140378b37dbb3e819c021ba081
Asim Mughal <asimkenya@gmail.com>
2018-10-09 17:58:30 +0300

    Version 1.0 - New Logos added

--- app/src/main/java/com/hotelaide/startup/LoginActivity.java
+++ app/src/main/java/com/hotelaide/startup/LoginActivity.java
@@ -89,7 +89,6 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
-
     // BASIC FUNCTIONS =============================================================================
     private void findAllViews() {
         viewPager = findViewById(R.id.view_pager);
@@ -99,6 +98,7 @@ public class LoginActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager, true);
 
     }
+//    private static float MIN_SCALE = 0.25f;
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
@@ -120,6 +120,42 @@ public class LoginActivity extends AppCompatActivity {
 
         viewPager.setAdapter(adapter);
         viewPager.setOffscreenPageLimit(5);
+
+// WHEN ADDING YOUR START UP PAGE...
+//        viewPager.setPageTransformer(false, new ViewPager.PageTransformer() {
+//            public void transformPage(@NonNull View view, float position) {
+//                int pageWidth = view.getWidth();
+//
+//                if (position < -1) { // [-Infinity,-1)
+//                    // This page is way off-screen to the left.
+//                    view.setAlpha(0);
+//
+//                } else if (position <= 0) { // [-1,0]
+//                    // Use the default slide transition when moving to the left page
+//                    view.setAlpha(1);
+//                    view.setTranslationX(0);
+//                    view.setScaleX(1);
+//                    view.setScaleY(1);
+//
+//                } else if (position <= 1) { // (0,1]
+//                    // Fade the page out.
+//                    view.setAlpha(1 - position);
+//
+//                    // Counteract the default slide transition
+//                    view.setTranslationX(pageWidth * -position);
+//
+//                    // Scale the page down (between MIN_SCALE and 1)
+//                    float scaleFactor = MIN_SCALE
+//                            + (1 - MIN_SCALE) * (1 - Math.abs(position));
+//                    view.setScaleX(scaleFactor);
+//                    view.setScaleY(scaleFactor);
+//
+//                } else { // (1,+Infinity]
+//                    // This page is way off-screen to the right.
+//                    view.setAlpha(0);
+//                }
+//            }
+//        });
 
     }
 
@@ -170,7 +206,6 @@ public class LoginActivity extends AppCompatActivity {
     public void navigateToSignUpScreen(View view) {
         viewPager.setCurrentItem(1);
     }
-
 
 
 }
